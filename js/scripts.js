1391dbc61a802a8928fcb1cee67185c73dc52616
gayu28 <srigayathri2812@gmail.com>
2022-11-30 23:28:36 +0530

    version-1: basic

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,18 @@
-/*!
-* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
+// @prepros-append vendor/jquery-3.3.1.min.js
+// @prepros-append vendor/popper.min.js
+// @prepros-append vendor/bootstrap.min.js
+// @prepros-append vendor/owl.carousel.min.js
+// @prepros-append vendor/aos.js
+// @prepros-append vendor/isotope.pkgd.min.js
+// @prepros-append vendor/jquery.animateNumber.min.js
+// @prepros-append vendor/jquery.waypoints.min.js
+// @prepros-append vendor/TweenMax.min.js
+// @prepros-append vendor/jquery.easing.1.3.js
+// @prepros-append vendor/jarallax.min.js
+// @prepros-append vendor/jarallax-video.min.js
+// @prepros-append vendor/jquery.validate.min.js
+// @prepros-append vendor/jquery.fancybox.min.js
+// @prepros-append vendor/stickyfill.min.js
+// @prepros-append vendor/imagesloaded.pkgd.min.js
+// @prepros-append vendor/ScrollMagic.min.js
+// @prepros-append vendor/scrollmagic.animation.gsap.min.js
